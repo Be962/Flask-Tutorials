c8d39439276d2d9741c616d44b90af3711b4fcf1
Be962 <benjamin.saunders23@education.nsw.gov.au>
2026-06-19 13:30:15 +1000

    Made a few more updates, finished migrations

--- app/models.py
+++ app/models.py
@@ -1,7 +1,13 @@
+from datetime import datetime, timezone
 from typing import Optional
 import sqlalchemy as sa
 import sqlalchemy.orm as so
 from app import db
+from werkzeug.security import generate_password_hash, check_password_hash
+from flask_login import UserMixin
+from app import login
+
+
 
 class User(db.Model):
     id: so.Mapped[int] = so.mapped_column(primary_key=True)
@@ -13,3 +19,29 @@ class User(db.Model):
 
     def __repr__(self):
         return '<User {}>'.format(self.username)
+    
+class Post(db.Model):
+    id: so.Mapped[int] = so.mapped_column(primary_key=True)
+    body: so.Mapped[str] = so.mapped_column(sa.String(140))
+    timestamp: so.Mapped[datetime] = so.mapped_column(
+        index=True, default=lambda: datetime.now(timezone.utc))
+    user_id: so.Mapped[int] = so.mapped_column(sa.ForeignKey(User.id),
+                                               index=True)
+
+    author: so.Mapped[User] = so.relationship(back_populates='posts')
+
+    def __repr__(self):
+        return '<Post {}>'.format(self.body)
+    
+
+class User(db.Model):
+
+    def set_password(self, password):
+        self.password_hash = generate_password_hash(password)
+
+    def check_password(self, password):
+        return check_password_hash(self.password_hash, password)
+    
+@login.user_loader
+def load_user(id):
+    return db.session.get(User, int(db))
--- app/routes.py
+++ app/routes.py
@@ -1,11 +1,39 @@
-from flask import render_template   
+from flask_login import current_user, login_user, logout_user
+from app import db
+from app.models import User
+import sqlalchemy as sa
+from flask import redirect, render_template, url_for, flash  
 from app import app
 from app.forms import LoginForm
+from flask_login import login_required
+from flask import request
+from urllib.parse import urlsplit
 
-@app.route('/login')
+
+@app.route('/logout')
+def logout():
+    logout_user()
+    return redirect(url_for('index'))
+
+@app.route('/login', methods=['GET', 'POST'])
 def login():
+    if current_user.is_authenticated:
+        return redirect(url_for('index'))
     form = LoginForm()
+    if form.validate_on_submit():
+        user = db.session.scalar(
+            sa.select(User).where(User.username == form.username.data))
+        if user is None or not user.check_password(form.password.data):
+            flash('Invalid username or password')
+            return redirect(url_for('login'))
+        login_user(user, remember=form.remember_me.data)
+        next_page = request.args.get('next')
+        if not next_page or urlsplit(next_page).netloc != '':
+            next_page = url_for('index')
+        return redirect(next_page)
+        return redirect(url_for('index'))
     return render_template('login.html', title='Sign In', form=form)
+
 @app.route('/')
 @app.route('/index')
 def index():
@@ -25,3 +53,9 @@ def index():
         }
     ]
     return render_template('index.html', title='Home', user=user, posts=posts)
+
+@app.route('/')
+@app.route('/index')
+@login_required
+def index():
+    return render_template("index.html", title='Home Page', posts=posts)
